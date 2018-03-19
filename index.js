cd9ba475238fb3f75b5ac798e204e0e10e2bda84
Jay <jvelayud@gmail.com>
2018-03-18 20:03:04 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const { json } = require("micro")
+const cors = require("micro-cors")()
+
+const list = [
+  "Install stuff",
+  "Look at the slides",
+  "Starting hacking"
+]
+
+module.exports = cors(async req => {
+  let body
+
+  switch (req.method) {
+    case "PUT":
+      body = await json(req)
+      list.push(body.text)
+      break
+    case "DELETE":
+      body = await json(req)
+      list.splice(body.id, 1)
+      break
+  }
+
+  return list
+})
